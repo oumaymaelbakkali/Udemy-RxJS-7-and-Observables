317a18fa9446113bb1a9c9bce4025598335d0af1
oumaymaelbakkali <93142901+oumaymaelbakkali@users.noreply.github.com>
2023-07-31 19:08:15 +0100

    Complete notification

--- external.ts
+++ external.ts
@@ -4,8 +4,20 @@ import { Observable } from "rxjs";
 const observable$ = new Observable<string>(subscriber => {
   console.log('Observable executed');
   subscriber.next('Alice');
+  subscriber.next('Ben');
+  setTimeout(() => {
+    subscriber.next('Charlie');
+    subscriber.complete();
+  }, 2000);
+
+  return () => {
+    console.log('Teardown');
+  };
 });
 
 console.log('Before subscribe');
-observable$.subscribe(value => console.log(value));
+observable$.subscribe({
+  next: value => console.log(value),
+  complete: () => console.log('Completed')
+});
 console.log('After subscribe');
