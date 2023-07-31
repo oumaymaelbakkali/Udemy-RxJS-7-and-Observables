f8b8018ed17076e567bfaaf4abd037facc0afbea
oumaymaelbakkali <93142901+oumaymaelbakkali@users.noreply.github.com>
2023-07-31 19:48:01 +0100

    Unsbscribe

--- external.ts
+++ external.ts
@@ -1,24 +1,22 @@
 import { Observable } from "rxjs";
 
 
-const observable$ = new Observable<string>(subscriber => {
-  console.log('Observable executed');
-  subscriber.next('Alice');
-  subscriber.next('Ben');
-  setTimeout(() => {
-    subscriber.next('Charlie');
+const interval$ = new Observable<number>(subscriber => {
+  let counter = 1;
+
+  const intervalId = setInterval(() => {
+    console.log('Emitted', counter);
+    subscriber.next(counter++);
   }, 2000);
-  setTimeout(() => subscriber.error(new Error('Failure')), 4000);
 
   return () => {
-    console.log('Teardown');
+    clearInterval(intervalId);
   };
 });
 
-console.log('Before subscribe');
-observable$.subscribe({
-  next: value => console.log(value),
-  error: err => console.log(err.message),
-  complete: () => console.log('Completed')
-});
-console.log('After subscribe');
+const subscription = interval$.subscribe(value => console.log(value));
+
+setTimeout(() => {
+  console.log('Unsubscribe');
+  subscription.unsubscribe();
+}, 7000);
