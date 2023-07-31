22e4f149e97a6076540136bd537ddbf7b54fe62e
oumaymaelbakkali <93142901+oumaymaelbakkali@users.noreply.github.com>
2023-07-31 19:45:46 +0100

    error notification

--- external.ts
+++ external.ts
@@ -7,8 +7,8 @@ const observable$ = new Observable<string>(subscriber => {
   subscriber.next('Ben');
   setTimeout(() => {
     subscriber.next('Charlie');
-    subscriber.complete();
   }, 2000);
+  setTimeout(() => subscriber.error(new Error('Failure')), 4000);
 
   return () => {
     console.log('Teardown');
@@ -18,6 +18,7 @@ const observable$ = new Observable<string>(subscriber => {
 console.log('Before subscribe');
 observable$.subscribe({
   next: value => console.log(value),
+  error: err => console.log(err.message),
   complete: () => console.log('Completed')
 });
 console.log('After subscribe');
