821bc3133a4b5ce207da08e4d03f9316834d624c
oumaymaelbakkali <93142901+oumaymaelbakkali@users.noreply.github.com>
2023-07-31 18:48:15 +0100

    Warm-up Observable

---
+++ index.ts
@@ -0,0 +1,24 @@
+import { Observable } from "rxjs";
+
+
+const observable$ = new Observable<string>(subscriber => {
+  console.log('Observable executed');
+  subscriber.next('Alice');
+  subscriber.next('Ben');
+  setTimeout(() => {
+    subscriber.next('Charlie');
+  }, 2000);
+  setTimeout(() => subscriber.error(new Error('Failure')), 4000);
+
+  return () => {
+    console.log('Teardown');
+  };
+});
+
+console.log('Before subscribe');
+observable$.subscribe({
+  next: value => console.log(value),
+  error: err => console.log(err.message),
+  complete: () => console.log('Completed')
+});
+console.log('After subscribe');
