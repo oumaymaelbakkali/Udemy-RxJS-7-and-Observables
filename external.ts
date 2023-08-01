ddc5532843a5dd820f755c656b5769f02be4ddbb
oumaymaelbakkali <93142901+oumaymaelbakkali@users.noreply.github.com>
2023-08-01 15:04:06 +0100

    tap

--- external.ts
+++ external.ts
@@ -1,24 +1,11 @@
+import { of } from "rxjs";
+import { filter, map, tap } from "rxjs/operators";
 
 
-import { forkJoin } from "rxjs";
-// Mike is from New Delhi and likes to eat pasta.
-
-import { ajax } from "rxjs/ajax";
-import { map } from "rxjs/operators";
-
-const randomFirstName$ = ajax<any>('https://random-data-api.com/api/name/random_name').pipe(
-  map(ajaxResponse => ajaxResponse.response.first_name)
-);
-
-const randomCapital$ = ajax<any>('https://random-data-api.com/api/nation/random_nation').pipe(
-  map(ajaxResponse => ajaxResponse.response.capital)
-);
-
-const randomDish$ = ajax<any>('https://random-data-api.com/api/food/random_food').pipe(
-  map(ajaxResponse => ajaxResponse.response.dish)
-);
-
-forkJoin([randomFirstName$, randomCapital$, randomDish$]).subscribe(
-  ([firstName, capital, dish]) =>
-    console.log(`${firstName} is from ${capital} and likes to eat ${dish}.`)
-);
+of(1, 7, 3, 6, 2).pipe(
+  filter(value => value > 5),
+  map(value => value * 2),
+  tap({
+    next: value => console.log('Spy:', value)
+  }),
+).subscribe(value => console.log('Output:', value));
